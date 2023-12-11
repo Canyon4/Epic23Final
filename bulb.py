47587016038726f0c97c9249a689b3e380c3b9f0
Canyon Sorrel <143111829+Canyon4@users.noreply.github.com>
2023-12-11 10:42:59 -0700

    Update bulb.py

--- bulb.py
+++ bulb.py
@@ -1,11 +1,13 @@
 import RPi.GPIO as GPIO
 import time 
+# imports GPIO and time to code
 green1 = 11
 green2 = 22
 blue1 = 15
 blue2 = 13
 red1 = 26
-red2 = 16
+red2 = 16 # sets up GPIO pins for lights
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)	
 GPIO.setup(green1,GPIO.OUT)
@@ -14,19 +16,20 @@ GPIO.setup(blue1,GPIO.OUT)
 GPIO.setup(blue2,GPIO.OUT)
 GPIO.setup(red1, GPIO.OUT)
 GPIO.setup(red2,GPIO.OUT)
-while True:
+
+while True: # runs a infinite loop
 	GPIO.output(blue1,GPIO.HIGH)
-	GPIO.output(blue2,GPIO.HIGH)
-	time.sleep(0.5)
+	GPIO.output(blue2,GPIO.HIGH) # turns on blue lights
+	time.sleep(0.5) # wait time
 	GPIO.output(blue1,GPIO.LOW)
-	GPIO.output(blue2,GPIO.LOW)
-	GPIO.output(green1,GPIO.HIGH)
-	GPIO.output(green2,GPIO.HIGH)
-	time.sleep(0.5)
-	GPIO.output(green1,GPIO.LOW)
+	GPIO.output(blue2,GPIO.LOW) # turns off blue lights
+	GPIO.output(green1,GPIO.HIGH) 
+	GPIO.output(green2,GPIO.HIGH) # turns on green lights
+	time.sleep(0.5) # wait time
+	GPIO.output(green1,GPIO.LOW) # turns off green lights
 	GPIO.output(green2,GPIO.LOW)
-	GPIO.output(red1, GPIO.HIGH)
+	GPIO.output(red1, GPIO.HIGH) # turns on red lights
 	GPIO.output(red2, GPIO.HIGH)
-	time.sleep(0.5)
-	GPIO.output(red1,GPIO.LOW)
+	time.sleep(0.5) # wait time
+	GPIO.output(red1,GPIO.LOW) # turn off red lights
 	GPIO.output(red2,GPIO.LOW)
